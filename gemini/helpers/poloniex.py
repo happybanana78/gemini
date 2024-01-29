4a6b56a293c36d3a385baa04ba365cc96d392b09
happybananadev <leonardo.mazzoleni@tecnasoft.it>
2024-01-29 07:42:18 +0100

    updated api calls 4

--- gemini/helpers/poloniex.py
+++ gemini/helpers/poloniex.py
@@ -36,15 +36,22 @@ def load_dataframe(pair, period, days_history=30):
     :param pair:
     :param period:
     :param days_history:
-    :param timeframe: H - hour, D - day, W - week, M - month
+    :param timeframe: MINUTE_1, MINUTE_5, MINUTE_10, MINUTE_15, MINUTE_30, HOUR_1, HOUR_2, HOUR_4, HOUR_6, HOUR_12, DAY_1, DAY_3, WEEK_1, MONTH_1
     :return:
     """
     data = get_past(pair, period, days_history)
     if 'error' in data:
         raise Exception("Bad response: {}".format(data['error']))
+    
+    """
+    Assign column names
+    """
+    column_names = ['low', 'high', 'open', 'close', 'amount', 'volume', 'buyTakerAmount', 'buyTakerVolume', 'tradeCount', 'date', 'weightedAverage', 'interval', 'startTime', 'closeTime']
 
-    df = pd.DataFrame(data)
-    df[9] = pd.to_datetime(df[9], unit='ms')
-    df = df.set_index([9])
+    df = pd.DataFrame(data, columns=column_names)
+    df['date'] = pd.to_datetime(df['date'], unit='ms')
+    df = df.set_index(['date'])
 
     return df
+
+load_dataframe('BTC_USDT', 'DAY_1', 100)
