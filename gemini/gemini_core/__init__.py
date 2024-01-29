94226735791fd1cb85ae232eb861b66782ad4784
happybananadev <leonardo.mazzoleni@tecnasoft.it>
2024-01-29 06:09:49 +0100

    pands sythax update

--- gemini/gemini_core/__init__.py
+++ gemini/gemini_core/__init__.py
@@ -4,5 +4,5 @@ from gemini.gemini_core import settings
 PRECISION = getattr(settings, "PRECISION", 8)
 
 # init pandas settings
-pd.set_option('precision', PRECISION)
+pd.set_option('display.precision', PRECISION)
 pd.set_option('display.float_format', lambda x: '%.{}f'.format(PRECISION) % x)
