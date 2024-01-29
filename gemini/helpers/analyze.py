0ecb6078de4151a4a2faa4355739bcf70049c793
happybananadev <leonardo.mazzoleni@tecnasoft.it>
2024-01-29 08:22:58 +0100

    hotfix

--- gemini/helpers/analyze.py
+++ gemini/helpers/analyze.py
@@ -40,7 +40,7 @@ def analyze_bokeh(algo, title=None, show_trades=False):
                 continue
 
             p.line(records['date'], records[c], color='grey',
-                   legend=c, y_range_name="Records")
+                   legend_label=c, y_range_name="Records")
 
     # print(algo.data[['date', 'close', 'sma50', 'sma150']])
 
@@ -53,11 +53,11 @@ def analyze_bokeh(algo, title=None, show_trades=False):
 
     p.line(algo.data.index, algo.data['base_equity'],
            color='#CAD8DE',
-           legend='Buy and Hold')
+           legend_label='Buy and Hold')
            # ,y_range_name="equity_y_axis")
     p.line(algo.data.index, algo.data['equity'],
            color='#49516F',
-           legend='Strategy')
+           legend_label='Strategy')
            #,y_range_name="equity_y_axis")
 
     cmo_above_50 = bokeh.models.Span(location=50,
